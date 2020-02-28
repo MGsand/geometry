1e89f6567e35ad1b7b6888bc93c54f4a55be9863
Майоров Глеб Аркадьевич <ip911s21@awp11.csc.local>
2020-02-28 11:03:19 +0700

    Правильная часть проги

--- geometry.c
+++ geometry.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main(){
-	int x,y,c;
-//	char st[8];
-	x=1+rand()%500;
-	y=1+rand()%500;
-	//printf("circle (%d  %d %d",x,y,);
+	int x=0,y=0,r=0;//i,m;
+	char st[6];
+	char st1[6]="circle";
+	scanf(" %s",st);
+	if (strcmp(st,st1)){
+	scanf("%d %d %d",&x,&y,&r);	
         printf("\n");  
-	c=1+rand()%20;
-        printf("circle (x=%d ,y=%d ,r=%d)",x,y,c);
-//	printf(" %d", c);
+	//c=1+rand()%20;
+        printf("circle (x=%d ,y=%d ,r=%d)",x,y,r);
+
+}
 	//system("pause");
 	return 0;
 }
